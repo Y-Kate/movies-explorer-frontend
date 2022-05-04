fa4e97cf1ea60cac07a403d586673df1e003cc99
Ekaterina Yadrikhinskaya <ya78087@yandex.ru>
2022-05-04 13:12:20 +0300

    feat: App наброски для верстки

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CurrentUserContext.Provider>
+      <div className="page">
+        Диплом о Себе
+        {/* <Header /> */}
+        <Switch>
+          <Route
+              exact 
+              path="/">
+          </Route>
+          <Route path="/sign-in">
+              Логин
+          </Route>
+          <Route path="/sign-up">
+            Пароль
+          </Route>
+        </Switch>
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
