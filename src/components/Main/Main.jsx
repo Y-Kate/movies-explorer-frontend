528d1c0608231f43f2db83e93ae70de79788d610
Ekaterina Yadrikhinskaya <ya78087@yandex.ru>
2022-05-05 10:54:46 +0300

    feat: AboutProject начало

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Promo from '../Promo/Promo';
+import AboutProject from '../AboutProject/AboutProject'
 import './Main.css'
 
 function Main() {
 
   return (
-
-  < Promo />
+  <div className="main">
+    < Promo />
+    < AboutProject />
+  </div>
   );
 }
 
