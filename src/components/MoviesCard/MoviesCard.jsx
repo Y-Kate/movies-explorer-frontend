3a374b6c070878490327e0801fc7ff4235423183
Ekaterina Yadrikhinskaya <ya78087@yandex.ru>
2022-05-10 17:04:45 +0300

    refactor: Movies адаптив

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,14 +1,55 @@
 import React from 'react';
-import kino from '../../images/kino.jpg'
-import kino2 from '../../images/kino2.jpg'
-import kino3 from '../../images/kino3.jpg'
 import './MoviesCard.css'
 
-function MoviesCard() {
-
+function MoviesCard({ filmData }) {
+  
   return (
     <>
       <li className="movies-card">
+        <a href={filmData.link} target="_blank" rel="noreferrer" className="movies-card__link">
+          <img src={filmData.img} alt="Обложка фильма" className="movies-card__poster"/>
+        </a>
+        <div className="movies-card__description">
+          <p className="movies-card__title">{filmData.name}</p>
+          <button type="button" className={`movies-card__like movies-card__like${filmData.isLiked ? '_active' : '_save'}`} aria-label="Нравится" ></button>
+        </div>
+        <hr className="movies-card__underline"/>
+        <p className="movies-card__duration">{filmData.time}</p>
+      </li>
+      {/* <li className="movies-card">
+        <a href="https://www.youtube.com/" target="_blank" rel="noreferrer" className="movies-card__link">
+          <img src={kino2} alt="Обложка фильма" className="movies-card__poster" />
+        </a>
+        <div className="movies-card__description">
+          <p className="movies-card__title">Бег это свобода</p>
+          <button type="button" className="movies-card__like movies-card__like_active" aria-label="Нравится" ></button>
+        </div>
+        <hr className="movies-card__underline"/>
+        <p className="movies-card__duration"> 1ч 49м</p>
+      </li>
+      <li className="movies-card">
+        <a href="https://www.youtube.com/" target="_blank" rel="noreferrer" className="movies-card__link">
+          <img src={kino3} alt="Обложка фильма" className="movies-card__poster" />
+        </a>
+        <div className="movies-card__description">
+          <p className="movies-card__title">Бег это свобода</p>
+          <button type="button" className="movies-card__like movies-card__like_save" aria-label="Нравится" ></button>
+        </div>
+        <hr className="movies-card__underline"/>
+        <p className="movies-card__duration"> 1ч 49м</p>
+      </li>
+      <li className="movies-card">
+        <a href="https://www.youtube.com/" target="_blank" rel="noreferrer" className="movies-card__link">
+          <img src={kino} alt="Обложка фильма" className="movies-card__poster" />
+        </a>
+        <div className="movies-card__description">
+          <p className="movies-card__title">Бег это свобода</p>
+          <button type="button" className="movies-card__like movies-card__like_save" aria-label="Нравится" ></button>
+        </div>
+        <hr className="movies-card__underline"/>
+        <p className="movies-card__duration"> 1ч 49м</p>
+      </li>
+      <li className="movies-card">
         <a href="https://www.youtube.com/" target="_blank" rel="noreferrer" className="movies-card__link">
           <img src={kino} alt="Обложка фильма" className="movies-card__poster"/>
         </a>
@@ -51,51 +92,7 @@ function MoviesCard() {
         </div>
         <hr className="movies-card__underline"/>
         <p className="movies-card__duration"> 1ч 49м</p>
-      </li>
-      <li className="movies-card">
-        <a href="https://www.youtube.com/" target="_blank" rel="noreferrer" className="movies-card__link">
-          <img src={kino} alt="Обложка фильма" className="movies-card__poster"/>
-        </a>
-        <div className="movies-card__description">
-          <p className="movies-card__title">Бег это свобода</p>
-          <button type="button" className="movies-card__like movies-card__like_save" aria-label="Нравится" ></button>
-        </div>
-        <hr className="movies-card__underline"/>
-        <p className="movies-card__duration"> 1ч 49м</p>
-      </li>
-      <li className="movies-card">
-        <a href="https://www.youtube.com/" target="_blank" rel="noreferrer" className="movies-card__link">
-          <img src={kino2} alt="Обложка фильма" className="movies-card__poster" />
-        </a>
-        <div className="movies-card__description">
-          <p className="movies-card__title">Бег это свобода</p>
-          <button type="button" className="movies-card__like movies-card__like_active" aria-label="Нравится" ></button>
-        </div>
-        <hr className="movies-card__underline"/>
-        <p className="movies-card__duration"> 1ч 49м</p>
-      </li>
-      <li className="movies-card">
-        <a href="https://www.youtube.com/" target="_blank" rel="noreferrer" className="movies-card__link">
-          <img src={kino3} alt="Обложка фильма" className="movies-card__poster" />
-        </a>
-        <div className="movies-card__description">
-          <p className="movies-card__title">Бег это свобода</p>
-          <button type="button" className="movies-card__like movies-card__like_save" aria-label="Нравится" ></button>
-        </div>
-        <hr className="movies-card__underline"/>
-        <p className="movies-card__duration"> 1ч 49м</p>
-      </li>
-      <li className="movies-card">
-        <a href="https://www.youtube.com/" target="_blank" rel="noreferrer" className="movies-card__link">
-          <img src={kino} alt="Обложка фильма" className="movies-card__poster" />
-        </a>
-        <div className="movies-card__description">
-          <p className="movies-card__title">Бег это свобода</p>
-          <button type="button" className="movies-card__like movies-card__like_save" aria-label="Нравится" ></button>
-        </div>
-        <hr className="movies-card__underline"/>
-        <p className="movies-card__duration"> 1ч 49м</p>
-      </li>
+      </li> */}
     </>
   );
 }
--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { filmsArray } from '../../utils/mock';
 import MoviesCard from '../MoviesCard/MoviesCard'
 import './MoviesCardList.css'
 
 function MoviesCardList() {
+  const [renderFilmsArray, setRenderFilmsArray] = useState([]);
+
+  useEffect(() => {
+    const windowSize = window.innerWidth;
+    let array = [];
+    if (windowSize > 1190) {
+      array = filmsArray.slice(0, 16);
+    } else if (windowSize > 619) {
+      array = filmsArray.slice(0, 8);
+    } else {
+      array = filmsArray.slice(0, 5);
+    };
+    console.log('array', array);
+    setRenderFilmsArray(array);
+    
+  }, [])
+  
 
   return (
     <section className="movies-list">
       <ul className="movies-list__container">
-        <MoviesCard />
+        {renderFilmsArray.map((film) => {
+          return <MoviesCard filmData={film}/>
+        })}
+        
       </ul>
       <button type="submit" className="movies-list__button">Ещё</button>
     </section>
